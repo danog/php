12ce14c13c6b0300128ca2cd345a049c8fb9515b
Daniil Gentili <daniil@daniil.it>
2023-05-30 19:49:12 +0200

    Alias

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ docker buildx create --use --name wp --driver remote tcp://192.168.69.206:1234
 
 build() {
 	cd "$1"
-	docker buildx build --pull --push --platform linux/riscv64 . -t danog/php:$2 --cache-from danog/php:$2 --cache-to type=inline
+	docker buildx build --pull --load --push --platform linux/riscv64 . -t danog/php:$2 --cache-from danog/php:$2 --cache-to type=inline
 	cd "$base"
 }
 
