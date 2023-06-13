33294bc54fe44331b4b05ea6d855a5cec679955e
Daniil Gentili <daniil@daniil.it>
2023-06-13 20:02:54 +0200

    Debug

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-#!/bin/sh -e
+#!/bin/sh -ex
 
 docker login --username "$DOCKER_USERNAME" --password "$DOCKER_PASSWORD"
 docker buildx create --use --name wp --driver remote tcp://192.168.69.206:1234
